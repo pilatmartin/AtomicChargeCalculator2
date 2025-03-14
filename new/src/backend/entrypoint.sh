ea462665f7c0c6593b56167d1ea6766a1b1098f4
unknown <8pilatmartin8@gmail.com>
2025-03-14 19:04:36 +0100

    feat(docker): add entrypoint script

---
+++ new/src/backend/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This script runs migrations before running the gunicorn server.
+# It is used in the Dockerfile.
+
+alembic upgrade head
+
+exec gunicorn --workers 2 --worker-class uvicorn.workers.UvicornWorker --timeout 600 --bind 0.0.0.0:8000 main:web_app
